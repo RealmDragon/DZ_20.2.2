d5558465266a26facbf3b71f26a0523ab685b4b1
RealmDragon <realmdragon2014@yandex.ru>
2024-09-08 15:44:01 +0300

    изменения от 08.09.24

--- catalog/product_views.py
+++ catalog/product_views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404
+from .models import Product
 
-from .models import Product  # Импортируйте модель товара
+def product_view(request, product_id):
+    """
+    Отображает страницу с информацией о конкретном продукте.
+    """
+    product = get_object_or_404(Product, pk=product_id)
 
-def product_detail(request, pk):
-    """
-    Отображает страницу с деталями товара.
-    """
-    product = get_object_or_404(Product, pk=pk)
     context = {
-        'product': product,
+        'product': product
     }
-    return render(request, 'products/product_detail.html', context)
+
+    return render(request, 'product/product_view.html', context)
--- catalog/urls.py
+++ catalog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),  # Главная страница
     path('products/', views.product_list, name='product_list'),  # Список товаров
     path('products/<int:pk>/', views.product_detail, name='product_detail'),  # Детальная страница товара
+    path('product/<int:product_id>/', views.product_view, name='product_view'),
     path('products/category/<slug:category_slug>/', views.product_list_by_category, name='product_list_by_category'),  # Список товаров по категории
 ]
